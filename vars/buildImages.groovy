6059f9351daaabf24b4ac1ff5eb9859580334d8c
Saad Ullah <saadullah3306@gmail.com>
2025-12-25 06:51:16 -0800

    Update Docker image tags for three-tier application

--- vars/buildImages.groovy
+++ vars/buildImages.groovy
@@ -1,6 +1,6 @@
 def call(String backendPath, String frontendPath, String imageTag) {
-    env.BACKEND_TAG_DH  = "${env.DOCKERHUB_USER}/three-tier-app-backend:${imageTag}"
-    env.FRONTEND_TAG_DH = "${env.DOCKERHUB_USER}/three-tier-app-frontend:${imageTag}"
+    env.BACKEND_TAG_DH  = "${env.DOCKERHUB_USER}/three-tier-application-backend:${imageTag}"
+    env.FRONTEND_TAG_DH = "${env.DOCKERHUB_USER}/three-tier-application-frontend:${imageTag}"
     sh """
         docker build -t ${BACKEND_TAG_DH} ${backendPath}
         docker build -t ${FRONTEND_TAG_DH} ${frontendPath}
